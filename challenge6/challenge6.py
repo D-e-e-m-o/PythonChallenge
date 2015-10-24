659120078fe7734014f2ec29ba28f81e5b53473d
D-e-e-m-o <wsd009811@gmail.com>
2015-10-24 17:08:33 +0800

    finish6

--- challenge6/challenge6.py
+++ challenge6/challenge6.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3.5
 
 import re,zipfile
 
@@ -8,7 +8,7 @@ zfile = zipfile.ZipFile('channel/channel.zip','r')
 comments = []
 for i in range(909):
     data = open('channel/'+num+'.txt').read()
-    comments.append(zfile.getinfo(num+'.txt').comment)
+    comments.append(zfile.getinfo(num+'.txt').comment.decode())
     #print(data)
     num = ''.join(re.findall(pattern,data))
 print(''.join(comments))
